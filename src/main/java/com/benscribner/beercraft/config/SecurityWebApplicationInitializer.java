e2df3c3af6008b0e50b916c595966bc94a91517d
Ben Scribner <justbsing@gmail.com>
2015-10-29 13:57:15 +0000

    added stuff

---
+++ src/main/java/com/benscribner/beercraft/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.benscribner.beercraft.configuration;
+ 
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+ 
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+ 
+}
